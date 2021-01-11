9f88c8b1f623498612600ea6aaa16075456714a4
PedroHenriqueBot06123 <77212398+PedroHenriqueBot06123@users.noreply.github.com>
2021-01-11 04:21:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,115 @@
+const
+{
+   WAConnection,
+   MessageType,
+   Presence,
+   MessageOptions,
+   Mimetype,
+   WALocationMessage,
+   WA_MESSAGE_STUB_TYPES,
+   ReconnectMode,
+   ProxyAgent,
+   GroupSettingChange,
+   waChatKey,
+   mentionedJid,
+   processTime,
+} = require("@adiwajshing/baileys")
+const qrcode = require("qrcode-terminal") 
+const moment = require("moment-timezone") 
+const fs = require("fs") 
+const { color, bgcolor } = require('./lib/color')
+const { help } = require('./lib/help')
+const { donasi } = require('./lib/donasi')
+const { fetchJson } = require('./lib/fetcher')
+const { recognize } = require('./lib/ocr')
+const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
+const tiktod = require('tiktok-scraper')
+const ffmpeg = require('fluent-ffmpeg')
+const { removeBackgroundFromImageFile } = require('remove.bg')
+const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
+const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
+const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
+const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + 'FN:Affis Admin\n' 
+            + 'ORG: Pengembang XBot;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'END:VCARD' 
+prefix = '!'
+blocked = []          
+
+/********** LOAD FILE **************/
+
+/********** END FILE ***************/
+  
+const time = moment().tz('Asia/Jakarta').format("HH:mm:ss")
+const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+const bulan = arrayBulan[moment().format('MM') - 1]
+const config = {
+    XBOT: '❉AmpibI❉', 
+    instagram: 'https://instagram.com/affis_saputro123', 
+    nomer: 'wa.me/6282334297175',
+    youtube: 'https://youtube.com/channel/UCGYLWtyT9IADYNUiK0uZiGg', 
+    whatsapp: 'Comming soon', 
+    tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
+    waktu: time
+}
+
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik`
+}
+
+
+const { tanggal, waktu, instagram, whatsapp, youtube, nomer, ontime } = config
+
+
+
+const { exec } = require("child_process")
+
+const client = new WAConnection()
+
+client.on('qr', qr => {
+   qrcode.generate(qr, { small: true })
+   console.log(`[ ${time} ] QR code is ready, subrek dulu yak ambipi team`)
+})
+
+client.on('credentials-updated', () => {
+   const authInfo = client.base64EncodedAuthInfo()
+   console.log(`credentials updated!`)
+
+   fs.writeFileSync('./session.json', JSON.stringify(authInfo, null, '\t'))
+})
+
+fs.existsSync('./session.json') && client.loadAuthInfo('./session.json')
+
+client.connect();
+
+// client.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log(`${time}: Bot by ig:@affis_saputro123`)
+
+client.on('group-participants-update', async (anu) => {
+		if (!welkom.includes(anu.jid)) return
+		try {
+			const mdata = await client.groupMetadata(anu.jid)
+			console.log(anu)
+			if (anu.action == 'add') {
+				num = anu.participants[0]
+				try {
+					ppimg = await client.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+				}
+				teks = `OII @${num.split('@')[0]}\ SEJA BEM VINDO (A) AO GRUPO *${mdata.subject}*`
+				let buff = await getBuffer(ppimg)
+				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
+			} else if (anu.action == 'remove') {
+				num = anu.participants[0]
+				try {
+					ppimg = await client.get
